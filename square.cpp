f8690f4c61a8d4f66c23a89ec06d2c1137a75430
MartaBardina <martaieva19@gmail.com>
2025-06-30 11:17:20 +0300

    Delete square.cpp

--- square.cpp
+++
@@ -1,8 +0,0 @@
-#include "square.h"
-
-Square::Square(int x, int y, QColor color) : Point(x, y, color) {}
-
-void Square::draw(QPainter* painter) {
-    painter->setBrush(color);
-    painter->drawRect(position.x() - 40, position.y() - 40, 80, 80);
-}
