dc65215bbb2cb1c47ded17764f61228040811f34
MartaBardina <martaieva19@gmail.com>
2025-06-30 11:15:51 +0300

    Delete circle.cpp

--- circle.cpp
+++
@@ -1,8 +0,0 @@
-#include "circle.h"
-
-Circle::Circle(int x, int y, QColor color) : Point(x, y, color) {}
-
-void Circle::draw(QPainter* painter) {
-    painter->setBrush(color);
-    painter->drawEllipse(position, 80, 80);
-}
